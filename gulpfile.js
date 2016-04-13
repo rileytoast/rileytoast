d718c267b01f1f30426969cef1b3bc30a3e2dbae
Riley Mondragon <rileytoast@gmail.com>
2016-04-13 16:40:55 -0500

    Gulp Build

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,27 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 var notify = require('gulp-notify');
- 
-gulp.task('less', function () {
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+var runSequence = require('run-sequence');
+
+gulp.task('coffee', function() {
+  gulp.src('./src/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./lib/'));
+});
+
+gulp.task('styles', function () {
   return gulp.src('./styles/**/index.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./dist/css'))
     .pipe(notify({ message: 'LESS task complete' }));
+});
+
+gulp.task('build', function () {
+  runSequence(['coffee', 'styles']);
 });
 
 // gulp.task('browserify', function () {
